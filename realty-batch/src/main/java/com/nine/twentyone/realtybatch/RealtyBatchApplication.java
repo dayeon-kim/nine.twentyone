e55f1276b6e875cacdad5042b85e37d833a94584
dayeon.kim <dayeon.kim@navercorp.com>
2017-07-30 21:06:47 +0900

    create batch project

---
+++ realty-batch/src/main/java/com/nine/twentyone/realtybatch/RealtyBatchApplication.java
@@ -0,0 +1,12 @@
+package com.nine.twentyone.realtybatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtyBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtyBatchApplication.class, args);
+	}
+}
---
+++ realty-batch/src/test/java/com/nine/twentyone/realtybatch/RealtyBatchApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nine.twentyone.realtybatch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class RealtyBatchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
